360f2df0c45389a9d257b771ee6f761488be3f4b
D2dSoft Developer <d2dsoftit@gmail.com>
2023-11-19 11:01:32 +0700

    u20231119

--- app/code/community/D2DSoft/DataMigration/controllers/Adminhtml/Dsdm/MigrationController.php
+++ app/code/community/D2DSoft/DataMigration/controllers/Adminhtml/Dsdm/MigrationController.php
@@ -93,6 +93,12 @@ class D2DSoft_DataMigration_Adminhtml_Dsdm_MigrationController
         if(!ini_get('allow_url_fopen')){
             $this->setMessage('error', 'The PHP "allow_url_fopen" must is enabled. Please follow <a href="https://www.a2hosting.com/kb/developer-corner/php/using-php.ini-directives/php-allow-url-fopen-directive" target="_blank">here</a> to enable the setting.');
         }
+        if (!extension_loaded('zip')) {
+            $this->setMessage('error', 'PHP Zip extension is not installed. Please install the Zip extension.');
+        }
+        /*if (!function_exists('eval')) {
+            $this->setMessage('error', 'Please enable the eval function.');
+        }*/
         $this->loadLayout();
         $this->_setActiveMenu('d2dsoft/datamigration')
             ->_title($this->getHelper()->__('Data Migration'));
@@ -133,6 +139,7 @@ class D2DSoft_DataMigration_Adminhtml_Dsdm_MigrationController
                 D2dInit::PROCESS_IMPORT,
                 D2dInit::PROCESS_RESUME,
                 D2dInit::PROCESS_REFRESH,
+                D2dInit::PROCESS_AUTH,
                 D2dInit::PROCESS_FINISH))){
             $this->responseJson(array(
                 'status' => 'error',
